886c9a9fefb41a46d99e801b0aeb523089d8851c
Henxun <1371490145@qq.com>
2021-06-29 12:39:12 +0800

    Excel Sheet Column Title

---
+++ ExcelSheetColumnTitle/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace ExcelSheetColumnTitle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ConvertToTitle(701)); 
+        }
+
+        static string ConvertToTitle(int columnNumber)
+        {
+            StringBuilder sb = new StringBuilder();
+            while(columnNumber > 0)
+            {
+                columnNumber--;
+                sb.Insert(0,(char)(columnNumber % 26 + 'A'));
+                columnNumber /= 26;
+            }
+            return sb.ToString();
+        }
+    }
+}
