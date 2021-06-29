26fc7a4dded494710bec99eed604c48437e37fad
Henxun <1371490145@qq.com>
2021-06-29 14:15:37 +0800

    MaximumSubarray

---
+++ MaximumSubarray/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MaximumSubarray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MaxSubArray(new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 }));
+        }
+        static int MaxSubArray(int[] nums)
+        {
+            int pre = 0, maxAns = nums[0];
+
+            foreach (var num in nums)
+            {
+                pre = Math.Max(pre + num, num);
+                maxAns = Math.Max(maxAns, pre); 
+            }
+            return maxAns;
+        }
+    }
+}
