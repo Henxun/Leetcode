abaa9ab221ab5532cd2983d30b64ff02e86d9c52
Henxun <1371490145@qq.com>
2021-06-30 13:51:40 +0800

    最后一个单词的长度

---
+++ LengthofLastWord/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LengthofLastWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(LengthOfLastWord("Hello"));
+        }
+
+        static int LengthOfLastWord(string s)
+        {
+            int length = s.Length, count = 0;
+            bool flag = false;
+            for (int i = length - 1; i >= 0; i--)
+            {
+                if(s[i] != ' ')
+                {
+                    flag = true;
+                    count++;
+                }
+                else
+                {
+                    if (flag) return count;
+                }
+            }
+            return count;
+        }
+    }
+}
