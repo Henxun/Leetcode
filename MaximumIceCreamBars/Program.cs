67ed7ca70c58244e9a4f06bb514745479fd67257
Henxun <1371490145@qq.com>
2021-07-02 10:29:55 +0800

    雪糕的最大数量

---
+++ MaximumIceCreamBars/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MaximumIceCreamBars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var costs = new int[]
+            {
+                1,3,2,4,1
+            };
+            int coins = 7;
+            Console.WriteLine(MaxIceCream(costs, coins));
+        }
+
+        static int MaxIceCream(int[] costs, int coins)
+        {
+            Array.Sort(costs);
+            int count = 0;
+            foreach (var cost in costs)
+            {
+                if (coins >= cost)
+                {
+                    count++;
+                    coins -= cost;
+                }
+                else
+                    break;
+            }
+            return count;
+        }
+    }
+}
