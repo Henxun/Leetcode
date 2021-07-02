91a18557b0230b8232c2d8d9b91a4d152b21a3c7
Henxun <1371490145@qq.com>
2021-07-02 11:07:41 +0800

    加一

---
+++ PlusOne/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PlusOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join(",",PlusOne(new int[]{ 9,9,9})));
+        }
+
+        static int[] PlusOne(int[] digits)
+        {
+            for (int i = digits.Length - 1; i >= 0; i--)
+            {
+                digits[i]++;
+                digits[i] = digits[i] % 10;
+                if(digits[i] != 0) return digits;
+            }
+
+            digits = new int[digits.Length + 1  ];
+            digits[0] = 1;
+            return digits;
+        }
+    }
+}
