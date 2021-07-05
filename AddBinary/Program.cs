c0cddc4c5caafac3638c228534ad9fa27cf44b50
Henxun <1371490145@qq.com>
2021-07-05 21:04:42 +0800

    二进制求和

---
+++ AddBinary/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+
+namespace AddBinary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AddBinary("11", "1"));
+        }
+
+        static string AddBinary(string a, string b)
+        {
+            int aLen = a.Length, bLen = b.Length, maxLen = Math.Max(aLen, bLen), carry = 0;
+            StringBuilder result = new StringBuilder();
+            for(int i = 0; i < maxLen; i++)
+            {
+                carry += i < aLen ? (a[aLen - i - 1] - '0') : 0;
+                carry += i < bLen ? (b[bLen - i - 1] - '0') : 0;
+
+                result.Insert(0, carry % 2);
+                carry = carry / 2;
+            }
+
+            if(carry > 0)
+            {
+                result.Insert(0, '1');
+            }
+
+            return result.ToString();
+        }
+    }
+}
