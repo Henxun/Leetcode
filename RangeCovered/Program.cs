50e43af81c841e792bb070cd09c590bcc3802f17
Henxun <1371490145@qq.com>
2021-07-23 16:34:42 +0800

    检查是否区域内所有整数都被覆盖

---
+++ RangeCovered/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace RangeCovered
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static bool IsCovered(int[][] ranges, int left, int right)
+        {
+            bool[] flags = new bool[51];
+
+            foreach (var i in ranges)
+            {
+                for(int j = i[0]; j <= i[1]; j++)
+                {
+                    flags[j] = true;
+                }
+            }
+
+            for(int i = left; i <= right; i++)
+            {
+                if (!flags[i])
+                    return false;
+            }
+            return true;
+        }
+    }
+}
