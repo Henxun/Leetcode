d64cb37c42eb959fc55fb104a79cc1b0f7e1973d
Henxun <1371490145@qq.com>
2021-07-27 12:58:28 +0800

    二叉树中第二小的节点

---
+++ RestoreTheArrayFromAdjacentPairs/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RestoreTheArrayFromAdjacentPairs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static int[] RestoreArray(int[][] adjacentPairs)
+        {
+
+        }
+    }
+}
---
+++ SecondMinimumNodeInABinaryTree/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace SecondMinimumNodeInABinaryTree
+{
+    class Program
+    {
+        static int ans = -1;
+        static int rootValue;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static void DFS(TreeNode node)
+        {
+            if(node == null)
+            {
+                return;
+            }
+
+            if(ans != -1 && node.val >= ans)
+            {
+                return;
+            }
+
+            if(node.val > rootValue)
+            {
+                ans = node.val;
+            }
+
+            DFS(node.left);
+            DFS(node.right);
+        }
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
