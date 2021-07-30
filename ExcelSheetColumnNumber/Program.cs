d53c44fcb5575da132f624c29f39c55d80e5badd
Henxun <1371490145@qq.com>
2021-07-30 23:35:43 +0800

    Excel 表列序号

---
+++ ExcelSheetColumnNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ExcelSheetColumnNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(TitleToNumber("AA"));
+        }
+
+        static int TitleToNumber(string columnTitle)
+        {
+            int length = columnTitle.Length, pow = 1, result = 0;
+
+            for(int i = length - 1; i >= 0; i--)
+            {
+                int num = columnTitle[i] - 'A' + 1;
+                result += num * pow;
+                pow *= 26;
+            }
+
+            return result;
+            
+        }
+    }
+}
