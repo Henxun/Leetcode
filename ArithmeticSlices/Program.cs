2fe082b762fff50e6df2e761efe5eb9a4ce7dd7c
Henxun <1371490145@qq.com>
2021-08-10 21:59:16 +0800

    等差数列划分

---
+++ ArithmeticSlices/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ArithmeticSlices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 1, 2, 3, 8, 9, 10 };
+            Console.WriteLine(NumberOfArithmeticSlices(nums));
+        }
+
+        static int NumberOfArithmeticSlices(int[] nums)
+        {
+            int n = nums.Length;
+            if (n == 1)
+                return 0;
+            
+            int d = nums[0] - nums[1], t = 0;
+            int ans = 0;
+            for(int i = 2; i< n; i++)
+            {
+                if(nums[i - 1] - nums[i] == d)
+                {
+                    ++t;
+                }
+                else
+                {
+                    d = nums[i - 1] - nums[i];
+                    t = 0;
+                }
+                ans += t;
+            }
+
+            return ans;
+        }
+    }
+}
