bb3c3e9a8ab618ab803929e7fa4eea0d9536b67e
Henxun <1371490145@qq.com>
2021-09-07 21:31:40 +0800

    分割平衡字符串

---
+++ SplitAStringInBalancedStrings/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SplitAStringInBalancedStrings
+{
+    //在一个 平衡字符串 中，'L' 和 'R' 字符的数量是相同的。
+    //给你一个平衡字符串 s，请你将它分割成尽可能多的平衡字符串。
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(BalancedStringSplit("RLRRLLRLRL"));
+        }
+
+        static int BalancedStringSplit(string s)
+        {
+            int ans = 0, d = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if(s[i] == 'L')
+                {
+                    d++;
+                }
+                else
+                {
+                    d--;
+                }
+                if(d == 0)
+                {
+                    ans++;
+                }
+            }
+            return ans;
+        }
+    }
+}
