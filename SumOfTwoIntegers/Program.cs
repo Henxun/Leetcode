195f0566b5402638f9d5c2e43f0c11bae7d0214f
Henxun <1371490145@qq.com>
2021-09-26 21:42:23 +0800

    两整数之和

---
+++ SumOfTwoIntegers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SumOfTwoIntegers
+{
+    //给你两个整数 a 和 b ，不使用 运算符 + 和 - ​​​​​​​，计算并返回两整数之和。
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetSum(2, 3));
+        }
+
+        static int GetSum(int a, int b)
+        {
+            while(b != 0)
+            {
+                int carry = (a & b) << 1;
+                a = a ^ b;
+                b = carry;
+            }
+            return a;
+        }
+    }
+}
