dc415f20f30f8cda58ce885027d2de4f41e19f26
Henxun <1371490145@qq.com>
2021-10-13 21:43:33 +0800

    Fizz Buzz

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FizzBuzz
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static IList<string> FizzBuzz(int n)
+        {
+            IList<string> list = new List<string>();
+            StringBuilder sb = new StringBuilder();
+            for (int i = 1; i <= n; i++)
+            {
+                if(i % 3 == 0)
+                {
+                    sb.Append("Fizz");
+                }
+
+                if(i % 5 == 0)
+                {
+                    sb.Append("Buzz");
+                }
+
+                if(sb.Length == 0)
+                {
+                    sb.Append(i);
+                }
+
+                list.Add(sb.ToString());
+                sb.Clear();
+            }
+            return list;
+        }
+    }
+}
