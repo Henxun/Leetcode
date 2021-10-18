5fafb249c82ea6d4a48bd700244c447d5ff975bf
Henxun <1371490145@qq.com>
2021-10-18 22:08:28 +0800

    数字的补数

---
+++ NumberComplement/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace NumberComplement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static int FindComplement(int num)
+        {
+            int highbit = 0;
+            for(int i = 1; i <= 30; i++)
+            {
+                if (num >= (1 << i))
+                {
+                    highbit = i;
+                }
+                else
+                    break;
+            }
+
+            //确保不会溢出
+            int mask = highbit == 30 ? 0x7fffffff : (1 << (highbit + 1)) - 1;
+            return num ^ mask;
+        }
+    }
+}
