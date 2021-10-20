3a7dc186a83bb35b16e14bfae690489351063c2d
Henxun <1371490145@qq.com>
2021-10-20 21:28:58 +0800

    最小操作次数使数组元素相等

---
+++ MinimumMovesToEqualArrayElements/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace MinimumMovesToEqualArrayElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static int MinMoves(int[] nums)
+        {
+            int min = nums.Min();
+            int res = 0;
+
+            foreach (int i in nums)
+            {
+                res += i - min;
+            }
+
+            return res;
+        }
+    }
+}
