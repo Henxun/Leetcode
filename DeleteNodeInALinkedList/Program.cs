5a44b4d77677ca149bade5de70c5939d42ffe852
Henxun <1371490145@qq.com>
2021-11-02 20:48:35 +0800

    删除链表中的节点

---
+++ DeleteNodeInALinkedList/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DeleteNodeInALinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static void DeleteNode(ListNode node)
+        {
+            node.val = node.next.val;
+            node.next = node.next.next;
+        }
+
+        public class ListNode
+        {
+            public int val { get; set; }
+            public ListNode next { get; set; }
+            public ListNode(int val)
+            {
+                this.val = val;
+            }
+        }
+    }
+}
